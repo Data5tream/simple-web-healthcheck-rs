8f92aac29b2d5433cd3a48b64d99311a28eeaf46
Data5tream <6423657+Data5tream@users.noreply.github.com>
2025-05-18 15:40:05 +0200

    Provide healthcheck URL via commandline argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use std::{env, process::exit};
+
+use reqwest::blocking::Client;
+
+fn create_client() -> Client {
+    Client::builder()
+        .timeout(std::time::Duration::from_secs(5))
+        .build()
+        .unwrap()
+}
+
 fn main() {
-    reqwest::blocking::get("http://127.0.0.1:8080/ping").unwrap_or_else(|_| {
-        std::process::exit(1);
-    });
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Missing URL\nUsage: simple-web-healthcheck <url>");
+        exit(1);
+    }
+
+    let path = &args[1];
+
+    let client = create_client();
+    let response = client.get(path).send().unwrap_or_else(|_| exit(1));
+
+    let status = response.status();
+
+    if status.is_client_error() || status.is_server_error() {
+        exit(1);
+    }
 }
