3be6d73f02e0a97bb15b77df5dc44b442534b7af
Simon Barth <sb@b1t.at>
2026-09-18 18:50:09 +0200

    feat: rewrite in std

---
+++ src/address.rs
@@ -0,0 +1,339 @@
+use std::{
+    error::Error,
+    fmt,
+    net::{SocketAddr},
+    str::FromStr,
+};
+
+pub struct HealthcheckAddr {
+    pub socket: SocketAddr,
+    pub path: String,
+}
+
+#[derive(Debug)]
+pub enum UrlError {
+    MissingArgument,
+    UnsupportedSchema,
+    HttpsNotSupported,
+    InvalidSocketAddr,
+}
+
+impl fmt::Display for UrlError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            UrlError::MissingArgument => write!(f, "missing url argument"),
+            UrlError::UnsupportedSchema => write!(f, "unsupported schema"),
+            UrlError::HttpsNotSupported => write!(f, "HTTPS is not supported"),
+            UrlError::InvalidSocketAddr => write!(f, "invalid socket address"),
+        }
+    }
+}
+
+impl Error for UrlError {}
+
+pub fn get_connection_url(args: &[String]) -> Result<HealthcheckAddr, UrlError> {
+    if args.len() != 2 {
+        eprintln!("Invalid arguments\nUsage: simple-web-healthcheck <URL>");
+        return Err(UrlError::MissingArgument);
+    }
+
+    let raw_url = &args[1];
+
+    if raw_url.len() > 8 && &raw_url[..8] == "https://" {
+        return Err(UrlError::HttpsNotSupported);
+    }
+
+    let mut addr_start = raw_url.find("://").unwrap_or_default();
+    if addr_start != 0 {
+        if raw_url.len() > 7 && &raw_url[..7] != "http://" {
+            return Err(UrlError::UnsupportedSchema);
+        }
+        // make sure we start after the `://` sequence
+        addr_start += 3;
+    }
+
+    let without_schema = &raw_url[addr_start..];
+
+    // offset the addr end by the start of the actual host part
+    let addr_end = without_schema.find('/').unwrap_or(without_schema.len());
+
+    let mut host_string = String::from(&without_schema[..addr_end]);
+    // this breaks on IPv6
+    if !host_string.contains(':') {
+        host_string.push_str(":80");
+    }
+
+    let path = if without_schema.len() > addr_end {
+        String::from(&without_schema[addr_end..])
+    } else {
+        String::from("/")
+    };
+
+    println!("{host_string}{path}");
+
+    let Ok(socket) = SocketAddr::from_str(&host_string) else {
+        println!("invalid host string: {host_string}");
+        return Err(UrlError::InvalidSocketAddr);
+    };
+
+    Ok(HealthcheckAddr { socket, path })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+    #[test]
+    fn missing_argument_returns_correct_error() {
+        let args = vec![String::from("cmd")];
+        let result = get_connection_url(&args);
+        assert!(result.is_err_and(|e| matches!(e, UrlError::MissingArgument)));
+    }
+
+    #[test]
+    fn https_is_not_supported() {
+        let args = vec![String::from("cmd"), String::from("https://test")];
+        let result = get_connection_url(&args);
+        assert!(result.is_err_and(|e| matches!(e, UrlError::HttpsNotSupported)));
+    }
+
+    #[test]
+    fn http_is_supported() {
+        let args = vec![String::from("cmd"), String::from("http://127.0.0.1")];
+        let result = get_connection_url(&args);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn schemas_besides_http_or_https_throw_generic_schema_error() {
+        let args = vec![String::from("cmd"), String::from("ftp://test")];
+        let result = get_connection_url(&args);
+        assert!(result.is_err_and(|e| matches!(e, UrlError::UnsupportedSchema)));
+    }
+
+    #[test]
+    fn ipv4_with_port_and_without_schema_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("127.0.0.1:3000")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv6_with_port_and_without_schema_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("[::1]:3000")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv4_without_port_schema_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("127.0.0.1")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv6_without_port_and_schema_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("[::1]")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv4_with_port_and_path_without_schema_resolves() {
+        let args = vec![String::from("cmd"), String::from("127.0.0.1:3000/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv6_with_port_and_path_without_schema_resolves() {
+        let args = vec![String::from("cmd"), String::from("[::1]:3000/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv4_with_path_and_without_port_schema_resolves() {
+        let args = vec![String::from("cmd"), String::from("127.0.0.1/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv6_with_path_and_without_port_and_schema_resolves() {
+        let args = vec![String::from("cmd"), String::from("[::1]/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv4_with_port_and_schema_without_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://127.0.0.1:3000")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv6_with_port_and_with_schema_and_without_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://[::1]:3000")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv4_with_schema_and_without_port_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://127.0.0.1")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv6_with_schema_and_without_port_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://[::1]")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/"));
+    }
+
+    #[test]
+    fn ipv4_with_port_and_schema_and_with_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://127.0.0.1:3000/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv6_with_port_schema_and_path_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://[::1]:3000/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 3000)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv4_with_path_and_schema_and_without_port_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://127.0.0.1/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+    #[test]
+    fn ipv6_with_path_and_schema_and_without_port_resolves() {
+        let args = vec![String::from("cmd"), String::from("http://[::1]/test")];
+        let result = get_connection_url(&args);
+
+        assert!(result.is_ok());
+        let socket = result.unwrap();
+        assert_eq!(
+            socket.socket,
+            SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 80)
+        );
+        assert_eq!(socket.path, String::from("/test"));
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,8 @@
-use http_body_util::Empty;
-use hyper::{Request, body::Bytes};
-use hyper_util::rt::TokioIo;
-use std::{env, process::exit, time::Duration};
-use tokio::{net::TcpStream, time::timeout};
+use std::{process::{ExitCode}};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let timeout_duration = Duration::from_secs(5);
+use simple_web_healthcheck::test_connection;
 
-    // Parse our URL...
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Missing URL\nUsage: simple-web-healthcheck <url>");
-        exit(1);
-    }
-    let url = &args[1].parse::<hyper::Uri>().unwrap_or_else(|_| {
-        eprintln!("Invalid URL: {}", &args[1]);
-        exit(2)
-    });
-
-    // Get the host and the port
-    let host = url.host().expect("uri has no host");
-    let port = url.port_u16().unwrap_or(80);
-
-    let address = format!("{host}:{port}");
-    println!("Connecting to {address}");
-
-    // Open a TCP connection to the remote host
-    let stream = match timeout(timeout_duration, TcpStream::connect(address)).await {
-        Ok(stream_result) => match stream_result {
-            Ok(stream) => stream,
-            Err(_) => exit(1),
-        },
-        Err(_) => exit(1),
-    };
-
-    // Create the Hyper client
-    let io = TokioIo::new(stream);
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            eprintln!("Connection failed: {err:?}");
-        }
-    });
-
-    // Create an HTTP request with an empty body and a HOST header
-    let authority = url.authority().unwrap().clone();
-    let req = Request::builder()
-        .uri(url)
-        .header(hyper::header::HOST, authority.as_str())
-        .body(Empty::<Bytes>::new())?;
-
-    // Perform the GET request
-    let status = match timeout(timeout_duration, sender.send_request(req)).await {
-        Ok(result) => match result {
-            Ok(result) => result.status(),
-            Err(_) => exit(1),
-        },
-        Err(_) => exit(1),
-    };
-
-    if status.is_client_error() || status.is_server_error() {
-        exit(4);
-    }
-
+fn main() -> Result<(), ExitCode> {
+    test_connection()?;
     Ok(())
 }
